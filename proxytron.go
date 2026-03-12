8a2f63e68cb97df28c5e087b4863324f213a7a83
reqster <reqster@yandex.ru>
2026-03-12 22:26:51 +0500

    Initial commit. Demo HTTP proxy server.

---
+++ proxytron.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	// a reliably HTTP-accepting website
+	target, _ := url.Parse("http://httpforever.com")
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	http.ListenAndServe(":8080", proxy)
+}
